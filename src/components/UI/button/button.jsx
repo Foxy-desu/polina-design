a05e3a277608b05a69e842eae154f2b1d4eda321
Alexandra <iammeduza@gmail.com>
2024-02-03 21:45:58 +0300

    feat: implement changable button

--- src/components/UI/button/button.jsx
+++ src/components/UI/button/button.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import styles from './button.module.scss'
+import styles from './Button.module.scss';
 
 const Button =(props)=> {
-    return <button className={`${styles.btn} ${styles.btn_colored_lg}`} type='button'>{props.value}</button>
-}
+    const {content, type=''} = props;
+    const additionalStyle = getStyle(checkType(type));
+
+    function checkType(type) {
+        const types = {
+            colored: 'btn_colored',
+            lg: 'btn_colored_lg'
+        };
+
+        for(let [key, value] of Object.entries(types)) {
+           if(key === type) return value;
+        }
+        return '';
+    };
+    function getStyle(type) {
+        if(type === '') return '';
+        return styles[type];
+    };
+    
+    return <button className={`${styles.btn}${' ' + additionalStyle}`} type='button'>{content}</button>
+};
 
 export {Button};
