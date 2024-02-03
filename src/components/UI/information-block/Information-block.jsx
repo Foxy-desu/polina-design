1730ec881f8b8bf53b2635e5083cb04a4d2e8ebd
Alexandra <iammeduza@gmail.com>
2024-02-03 21:46:29 +0300

    feat: add information-block module

--- src/components/UI/information-block/Information-block.jsx
+++ src/components/UI/information-block/Information-block.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import * as DOMPurify from 'dompurify';
+import parse from 'html-react-parser';
 import styles from './Information-block.module.scss';
 
 const InformationBlock = (props) => {
     const {heading, description} = props;
-
     function wrapInSpan(description) {
         const regex = /[0-9]+%/g;
         const result = description.replace(regex, `<span class=${styles.accent}>$&</span>`);
         return result;
-    }
+    };
+    const pure = DOMPurify.sanitize(wrapInSpan(description));
 
     return <div className={styles['information-block']}>
         <h3 className={styles['information-block__heading']}>{heading}</h3>
-        <p className={styles['information-block__desc']} dangerouslySetInnerHTML={{__html: wrapInSpan(description)}}></p>
+        <p className={styles['information-block__desc']}>{parse(pure)}</p>
     </div>
 }
 
