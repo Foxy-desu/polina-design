214bcb4d7f4c83988b0f0d305c9796ce5932a6b5
Alexandra <iammeduza@gmail.com>
2024-02-03 15:53:42 +0300

    feat: add assets to the project

---
+++ src/components/UI/information-block/Information-block.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from './Information-block.module.scss';
+
+const InformationBlock = (props) => {
+    const {heading, description} = props;
+
+    function wrapInSpan(description) {
+        const regex = /[0-9]+%/g;
+        const result = description.replace(regex, `<span class=${styles.accent}>$&</span>`);
+        return result;
+    }
+
+    return <div className={styles.information-block}>
+        <h3 className={styles.information-block__heading}>{heading}</h3>
+        <p className={styles.information-block__desc} dangerouslySetInnerHTML={{__html: wrapInSpan(description)}}></p>
+    </div>
+}
+
+export {InformationBlock};
