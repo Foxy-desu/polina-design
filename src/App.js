15d94b82e23427ddde0cdcffcbf75d594254156e
Alexandra <iammeduza@gmail.com>
2024-02-03 17:12:56 +0300

    feat: add current  component for dev purposes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import {InformationBlock} from './components/UI/information-block/Information-block';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <InformationBlock heading='oh wow!' description='get a perfect 70% discount!'/>
     </div>
   );
 }
