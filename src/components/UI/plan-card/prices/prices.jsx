d1e32c03342c9f0edeac8a2976fb79866c09b6f6
Alexandra <iammeduza@gmail.com>
2024-02-05 20:51:19 +0300

    feat: add prices component

---
+++ src/components/UI/plan-card/prices/prices.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./prices.module.scss";
+
+const Prices = (props) => {
+
+    const {price, prevPrice} = props;
+
+    return <div className={styles["prices"]}>
+        <p className={styles["prices__current"]}>{price + ' rub'}</p>
+        {prevPrice ? <p className={styles["prices__previous"]}>{prevPrice + ' rub'}</p> : false}
+    </div>
+};
+
+export {Prices};
