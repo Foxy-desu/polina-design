8c8ea139dcf0484fc28a780f11b5443ed1325be1
Alexandra <iammeduza@gmail.com>
2024-02-05 20:46:07 +0300

    feat: move list block to a separate component

--- src/components/UI/plan-card/Plan-card.jsx
+++ src/components/UI/plan-card/Plan-card.jsx
@@ -1,32 +1,39 @@
 import React from "react";
-import {Button} from "../button/button";
-import { PerksListItem } from "./perks-list-item/Perks-list-item";
-import styles from './Plan-card.module.scss';
+import { CardHeader } from "./card-header/card-header.jsx";
+import { Button } from "../button/button.jsx";
+import { CardList } from "./card-list/card-list.jsx";
+import { Prices } from "./prices/prices.jsx";
+import styles from './plan-card.module.scss';
 
 //TODO: implement styles
 
 const PlanCard = (props)=> {
-    const {plan, perks, price, prevPrice} = props.data;
+    const {plan, perks, price, prevPrice, discount} = props.data;
+    const type = props.type;
 
-    function createComponentKey(str) {
-        return str.split('').reduce((prevHash, currVal) =>
-          (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
-    };
-
-    function listPerks(perks) {
-        return perks.map((perk)=> {
-            const hashed = createComponentKey(perk);
-            return <PerksListItem content={perk} key={hashed}/> 
-        })
-    };
-
-    return <article>
-        <h3>{plan}</h3>
-        <ul>{listPerks(perks)}</ul>
-        <p>{price}</p>
-        <p>{prevPrice}</p>
-        <Button content='заказать' type='lg'/>
-    </article>
+    return (
+    <article className={
+        `${styles["plan-card"]}
+         ${type.includes('light')
+         ? styles["plan-card_light"]
+         : styles["plan-card_dark"]}
+         ${type.includes('glowing')
+         ? ' ' + styles["plan-card_glowing"]
+         : false}`
+        }>
+        <div className={styles["plan-card__card-header"]}>
+            <CardHeader content={plan} discount={discount}/>
+        </div>
+        <div className={styles["plan-card__card-list"]}>
+            <CardList content={perks}/>
+        </div>
+        <div className={styles["plan-card__card-prices"]}>
+            <Prices price={price} prevPrice={prevPrice}/>
+        </div>
+        <div className={styles["plan-card__card-btn"]}>
+            <Button content='заказать' type='lg'/>
+        </div>
+    </article>)
 }
 
 export {PlanCard};
