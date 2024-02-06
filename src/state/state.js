a58a26d503ebf5d7eaeee39ba00a0666fe378134
Alexandra <iammeduza@gmail.com>
2024-02-06 21:37:41 +0300

    feat: implement state file

---
+++ src/state/state.js
@@ -0,0 +1,28 @@
+const state = {
+    socials: [
+        {name: 'vkontakte', link: 'https://vk.com/plndsg'},
+        {name: 'instagram', link: 'https://www.instagram.com/'},
+        {name: 'telegram', link: 'https://web.telegram.org/'},
+    ],
+    portfolioCards: [
+        {id: 1, info: 'Креатин мохито', path: 'images/portfolioImages/1.png'},
+        {id: 2, info: 'Игра тренажер', path: 'images/portfolioImages/2.png'},
+        {id: 3, info: 'Корзина для белья', path: 'images/portfolioImages/3.png'},
+        {id: 4, info: 'Стимулятор для клитора', path: 'images/portfolioImages/4.png'},
+        {id: 5, info: 'Сублимированная малина', path: 'images/portfolioImages/5.png'},
+        {id: 6, info: 'Бесщеточный шуруповерт', path: 'images/portfolioImages/6.png'},
+        {id: 7, info: 'Православный крестик', path: 'images/portfolioImages/7.png'},
+        {id: 8, info: 'Настольная лампа', path: 'images/portfolioImages/8.png'},
+        {id: 9, info: 'Гель для стирки', path: 'images/portfolioImages/9.png'},
+        {id: 10, info: 'Защитные уголки', path: 'images/portfolioImages/10.png'},
+        {id: 11, info: 'Амбиентная подсветка', path: 'images/portfolioImages/11.png'},
+        {id: 12, info: 'Глюкозамин-хондроитин', path: 'images/portfolioImages/12.png'},
+        {id: 13, info: 'Л-карнитин концентрат', path: 'images/portfolioImages/13.png'},
+        {id: 14, info: 'Вешалка-трансформер', path: 'images/portfolioImages/14.png'},
+        {id: 15, info: '3D EVA коврики', path: 'images/portfolioImages/15.png'},
+        {id: 16, info: 'Ретон-форте', path: 'images/portfolioImages/16.png'},
+    ]
+
+};
+
+export {state};
