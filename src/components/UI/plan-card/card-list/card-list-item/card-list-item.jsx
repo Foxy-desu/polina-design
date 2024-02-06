1531ee22fa2002eb77efff0b7d866ca2dafbff23
Alexandra <iammeduza@gmail.com>
2024-02-06 21:35:57 +0300

    feat: implement line break

--- src/components/UI/plan-card/card-list/card-list-item/card-list-item.jsx
+++ src/components/UI/plan-card/card-list/card-list-item/card-list-item.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import parse from 'html-react-parser';
 import styles from "./card-list-item.module.scss";
 
-//TODO: implement line break after colon
 const CardListItem = (props)=> {
     const {content} = props;
-    
-    return <li className={styles["card-list__item"]}>{content}</li>;
+    function addLineBreak(itemContent) {
+        const formated = itemContent.includes(':')
+        ? parse(itemContent.replace(': ', ':<br/>'))
+        : itemContent;
+
+        return formated;
+    };
+    return <li className={styles["card-list__item"]}>{addLineBreak(content)}</li>;
 }
 export {CardListItem}
