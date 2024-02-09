af2facbe9c2d0c2ff1e987829e2e6d94fc1e1c87
Alexandra <iammeduza@gmail.com>
2024-02-09 21:45:55 +0300

    feat: add image module

---
+++ src/components/image/image.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./image.module.scss";
+import heroImage from "../../assets/images/hero-img.svg";
+
+
+export const Image = (props) => {
+
+    return (
+        < div className={styles["image"]}>
+            <img className={styles["image__main-text"]} src={heroImage} alt="Эффектный дизайн"/>
+            <div className={styles["image__block-wrap"]}>
+                <div className={styles["image__add-block"]}>DИZАЙN</div>
+            </div>
+        </div>
+    )
+}
