45c6c94b4f81fa5ce19531dc46c4403469964fd5
Alexandra <iammeduza@gmail.com>
2024-02-09 21:54:40 +0300

    fix: implement correct style appliance

--- src/components/UI/photo-frame/photo-frame.jsx
+++ src/components/UI/photo-frame/photo-frame.jsx
@@ -7,28 +7,26 @@ const PhotoFrame = ({path, info, type}) => {
 
     function setStyle(type) {
         const types = {
-            "designed": "photo-frame__image_designed",
-            "plain": "photo-frame__image_plain",
+            "designed": "photo-frame_designed",
+            "plain": "photo-frame_plain",
         };
 
         for(let [key, value] of Object.entries(types)) {
-            if(key === type) return styles[value];
+            if(type === key) return styles[value];
         };
         return "";
     }
-
     
-    if(error) return <img src="path" alt="info"/>
+    if(error) return <img src={path} alt={info}/>
     return (
-        <div className={styles["photo-frame"]}>
+        <div className={`${styles["photo-frame"]}${' ' + setStyle(type)}`}>
            {loading ? (
             <div>{'loading'}</div>
             ) : (
-
                 <div className={styles["photo-frame__wrap"]}>
-                    <img className={`${styles["photo-frame__image"]}${' ' + setStyle(type)}`} width={167} height={223} src={image} alt={info}/>
+                    <img className={styles["photo-frame__image"]} width={167} height={223} src={image} alt={info}/>
                 </div>
-            )
+                )
             }
         </div>
     )
