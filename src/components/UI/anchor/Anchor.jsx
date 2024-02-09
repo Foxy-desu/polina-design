e81bf3b2cf8db6459fcaf0e64dea6f8a182b2b56
Alexandra <iammeduza@gmail.com>
2024-02-09 21:52:53 +0300

    fix: change active state implementation

--- src/components/UI/anchor/Anchor.jsx
+++ src/components/UI/anchor/Anchor.jsx
@@ -1,11 +1,12 @@
-import styles from './Anchor.module.scss';
+import { useState } from 'react';
+import styles from './anchor.module.scss';
 
 const Anchor = (props) => {
     //activate .anchor_active using js//
     //activate on tabulation enter
-    const {sectionId, content, isActive} = props;
-
-    return <div className={`${isActive === 'true'? styles.anchor + ' ' + styles.anchor_active : styles.anchor}`}>
+    const {sectionId, content, id, clickedId, setClickedId} = props;
+    
+    return <div className={`${clickedId === id? styles.anchor + ' ' + styles.anchor_active : styles.anchor}`} onClick={()=> setClickedId(id)}>
         <a className={styles.anchor__link} href={`#${sectionId}`}>{content}</a>
     </div>
 }
