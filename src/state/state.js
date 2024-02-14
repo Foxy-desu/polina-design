cf62598f292422f551a469820e3324e789f9c8b7
Alexandra <iammeduza@gmail.com>
2024-02-14 15:27:51 +0300

    feat: add portfolio section block

--- src/state/state.js
+++ src/state/state.js
@@ -4,24 +4,6 @@ const state = {
         {name: 'instagram', link: 'https://www.instagram.com/'},
         {name: 'telegram', link: 'https://web.telegram.org/'},
     ],
-    portfolioCards: [
-        {id: 1, info: 'Креатин мохито', path: 'images/portfolioImages/1.png'},
-        {id: 2, info: 'Игра тренажер', path: 'images/portfolioImages/2.png'},
-        {id: 3, info: 'Корзина для белья', path: 'images/portfolioImages/3.png'},
-        {id: 4, info: 'Стимулятор для клитора', path: 'images/portfolioImages/4.png'},
-        {id: 5, info: 'Сублимированная малина', path: 'images/portfolioImages/5.png'},
-        {id: 6, info: 'Бесщеточный шуруповерт', path: 'images/portfolioImages/6.png'},
-        {id: 7, info: 'Православный крестик', path: 'images/portfolioImages/7.png'},
-        {id: 8, info: 'Настольная лампа', path: 'images/portfolioImages/8.png'},
-        {id: 9, info: 'Гель для стирки', path: 'images/portfolioImages/9.png'},
-        {id: 10, info: 'Защитные уголки', path: 'images/portfolioImages/10.png'},
-        {id: 11, info: 'Амбиентная подсветка', path: 'images/portfolioImages/11.png'},
-        {id: 12, info: 'Глюкозамин-хондроитин', path: 'images/portfolioImages/12.png'},
-        {id: 13, info: 'Л-карнитин концентрат', path: 'images/portfolioImages/13.png'},
-        {id: 14, info: 'Вешалка-трансформер', path: 'images/portfolioImages/14.png'},
-        {id: 15, info: '3D EVA коврики', path: 'images/portfolioImages/15.png'},
-        {id: 16, info: 'Ретон-форте', path: 'images/portfolioImages/16.png'},
-    ],
     introCards: {
         plain: {
             id: 17, info: "Карточка до обработки", path: "images/plain-card-1.png"
@@ -90,6 +72,31 @@ const state = {
                 type: "dark, glowing"
             },
         ]
+    },
+    portfolioSection: {
+        sectionHeading: {content:"наши работы", type: "2"},
+        portfolioCards: [
+            {id: "port1", info: 'Креатин мохито', path: 'images/portfolioImages/1.png'},
+            {id: "port2", info: 'Сублимированная малина', path: 'images/portfolioImages/5.png'},
+            {id: "port3", info: 'Гель для стирки', path: 'images/portfolioImages/9.png'},
+            {id: "port4", info: 'Л-карнитин концентрат', path: 'images/portfolioImages/13.png'},
+            {id: "port5", info: 'Игра тренажер', path: 'images/portfolioImages/2.png'},
+            {id: "port6", info: 'Бесщеточный шуруповерт', path: 'images/portfolioImages/6.png'},
+            {id: "port7", info: 'Защитные уголки', path: 'images/portfolioImages/10.png'},
+            {id: "port8", info: 'Вешалка-трансформер', path: 'images/portfolioImages/14.png'},
+            {id: "port9", info: 'Корзина для белья', path: 'images/portfolioImages/3.png'},
+            {id: "port10", info: 'Православный крестик', path: 'images/portfolioImages/7.png'},
+            {id: "port11", info: 'Амбиентная подсветка', path: 'images/portfolioImages/11.png'},
+            {id: "port12", info: '3D EVA коврики', path: 'images/portfolioImages/15.png'},
+            {id: "port13", info: 'Стимулятор для клитора', path: 'images/portfolioImages/4.png'},
+            {id: "port14", info: 'Настольная лампа', path: 'images/portfolioImages/8.png'},
+            {id: "port15", info: 'Глюкозамин-хондроитин', path: 'images/portfolioImages/12.png'},
+            {id: "port16", info: 'Ретон-форте', path: 'images/portfolioImages/16.png'},
+        ],
+        buttons: [
+            {id: "portBtn1", content: "полный список цен", type: "", action: ""},
+            {id: "portBtn2", content: "сделать заказ", type: "colored", action: ""}
+        ],
     }
 
 };
