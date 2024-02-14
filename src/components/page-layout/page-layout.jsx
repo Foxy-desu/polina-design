636b6ab58a335d4dd6eb7982d611c1418f60d9c4
Alexandra <iammeduza@gmail.com>
2024-02-14 13:16:39 +0300

    fix: replace blocks of code with components along with edditing styles

--- src/components/page-layout/page-layout.jsx
+++ src/components/page-layout/page-layout.jsx
@@ -3,12 +3,16 @@ import { NavigationBar } from "../navigation-bar/navigation-bar.jsx";
 import { SocialList } from "../UI/social-list/social-list.jsx";
 import { Image } from "../image/image.jsx";
 import { Intro } from "../intro/intro.jsx";
+import { AdvantagesSec } from "../advantages-section/advantages-section.jsx";
 import styles from "./page-layout.module.scss";
+import { PricesSection } from "../prices-section/prices-section.jsx";
 
 
 const PageLayout = ({data}) => {
     const socials = data.socials;
     const intro = data.introCards;
+    const advantages = data.advantagesSection
+    const prices = data.pricesSection;
     const infoCardText = {
         heading: "кликабельная",
         description: "обложка это 60% успеха!"
@@ -16,74 +20,56 @@ const PageLayout = ({data}) => {
 
     return (
         <div className={styles["page-layout"]}>
-            <header className={styles["header"]}>
-                <div className={styles["horizontal-wrap"]}>
-                    <aside className={`${styles["aside"]} ${styles["nav-wrap"]}`}>
-                        <NavigationBar/>
-                    </aside>                
-                    <div className={styles["hero"]}>
-                        <div className={styles["hero__image-wrap"]}>
-                            <Image/>
-                        </div>
-                        <div className={styles["hero__intro-wrap"]}>
-                            <Intro cardsData={intro} infoBlockData={infoCardText}/>
-                        </div>
-                    </div>
-                    <aside className={`${styles["aside"]} ${styles["socials-wrap"]}`}>
-                        <SocialList data={socials}/>
-                    </aside>
-                </div>
-            </header>
-            <main className="main">
-                <section className="advantages" id="advantages">
-                    <div className={styles["content-wrap"]}>
-                        <h2>we are better</h2>
-                        <div>
-                            infoblock
-                            infoblock
-                            infoblock
-                            infoblock
+            <aside className={`${styles["page-layout__aside"]} ${styles["nav-wrap"]}`}>
+                <NavigationBar/>
+            </aside>
+            <div className="page-layout__content">
+                <header className={styles["header"]}>
+                    <div className={styles["horizontal-wrap"]}>
+                        <div className={styles["hero"]}>
+                            <div className={styles["hero__image-wrap"]}>
+                                <Image/>
+                            </div>
+                            <div className={styles["hero__intro-wrap"]}>
+                                <Intro cardsData={intro} infoBlockData={infoCardText}/>
+                            </div>
                         </div>
                     </div>
-                </section >
-                <section className="prices" id="prices">
-                    <div className={styles["content-wrap"]}>
-                        <h2>prices</h2>
-                        <div>
-                            card
-                            card
-                            card
+                </header>
+                <main className="main">
+                    <AdvantagesSec advantagesData={advantages}/>
+                    <PricesSection pricesData={prices} />
+                    <section className="portfolio" id="portfolio">
+                        <div className={styles["content-wrap"]}>
+                            <h2>our works</h2>
+                            <div>cards...</div>
+                            <div>buttons...</div>
                         </div>
-                        button
-                    </div>
-                </section>
-                <section className="portfolio" id="portfolio">
-                    <div className={styles["content-wrap"]}>
-                        <h2>our works</h2>
-                        <div>cards...</div>
-                        <div>buttons...</div>
-                    </div>
-                </section>
-                <section className="atmosphere" id="atmosphere">
-                    <div className={styles["content-wrap"]}>
-                        <h2>not just an image</h2>
-                        <div>
-                            infoblock
-                            images
-                            logo-decoration
+                    </section>
+                    <section className="atmosphere" id="atmosphere">
+                        <div className={styles["content-wrap"]}>
+                            <h2>not just an image</h2>
+                            <div>
+                                infoblock
+                                images
+                                logo-decoration
+                            </div>
+                            <div>
+                                images
+                                infoblock
+                                images
+                                phones
+                            </div>
                         </div>
-                        <div>
-                            images
-                            infoblock
-                            images
-                            phones
-                        </div>
-                    </div>
-                </section>
-            </main>
-            <footer>
+                    </section>
+                </main>
+                <footer>
                 contacts
-            </footer>
+                </footer>
+            </div>
+            <aside className={`${styles["page-layout__aside"]} ${styles["socials-wrap"]}`}>
+                <SocialList data={socials}/>
+            </aside>
         </div>
     )
 };
