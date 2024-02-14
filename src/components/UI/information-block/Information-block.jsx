d4ca0be5114b617de10a2ae5966ea8d182c7e439
Alexandra <iammeduza@gmail.com>
2024-02-14 13:05:17 +0300

    feat: add line break function to the component, add large modifier

--- src/components/UI/information-block/Information-block.jsx
+++ src/components/UI/information-block/Information-block.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import * as DOMPurify from 'dompurify';
 import parse from 'html-react-parser';
-import styles from './Information-block.module.scss';
+import styles from './information-block.module.scss';
 
-const InformationBlock = (props) => {
-    const {heading, description} = props;
+const InformationBlock = ({heading, description, descType="small"}) => {
     function wrapInSpan(description) {
         const regex = /[0-9]+%/g;
         const result = description.replace(regex, `<span class=${styles.accent}>$&</span>`);
         return result;
     };
-    const pure = DOMPurify.sanitize(wrapInSpan(description));
+
+    function addLineBreak(itemContent) {
+        const formated = itemContent.includes('\n')
+        ? itemContent.replace('\n', '<br/>')
+        : itemContent;
+
+        return formated;
+    };
+
+    const pure = DOMPurify.sanitize(addLineBreak(wrapInSpan(description)));
 
     return <div className={styles['information-block']}>
         <h3 className={styles['information-block__heading']}>{heading}</h3>
-        <p className={styles['information-block__desc']}>{parse(pure)}</p>
+        <p className={`${styles['information-block__desc']}${descType === "large" ? " " + styles['information-block__desc_lg'] : ""}`}>{parse(pure)}</p>
     </div>
 }
 
