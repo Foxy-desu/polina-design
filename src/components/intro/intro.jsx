f3d1b160dbd374d359aed47d8326a106c0d66cd3
Alexandra <iammeduza@gmail.com>
2024-02-14 13:00:00 +0300

    feat: add components to the intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./intro.module.scss";
 import { PhotoFrame } from "../UI/photo-frame/photo-frame";
+import { EmojiCard } from "../UI/emoji-card/emoji-card";
+import { InformationBlock } from "../UI/information-block/information-block";
 
 export const Intro =({cardsData, infoBlockData})=> {
     const plain = cardsData.plain;
@@ -13,15 +15,19 @@ export const Intro =({cardsData, infoBlockData})=> {
                 <div className={styles["intro__content"]}>
                     <div className={styles["cards"]}>
                         <div className={`${styles["cards__plain-card"]} ${styles["card-wrap"]}`}>
+                            <EmojiCard type="plain" content="до"/>
                             <PhotoFrame path={plain.path} info={plain.info} type='plain'/>
                         </div>
+                        <div className={styles["cards__transform-arrow"]}>
+                        </div>
                         <div className={`${styles["cards__designed-card"]} ${styles["card-wrap"]}`}>
+                            <EmojiCard type="designed" content="после"/>
                             <PhotoFrame path={designed.path} info={designed.info} type='designed'/>
                         </div>
                     </div>
                     <div className={styles["intro__info-wrap"]}>
-                        infoblock
-                        arrow
+                        <InformationBlock heading={infoBlockData.heading} description={infoBlockData.description} descType="large"/>
+                        <div className={styles["intro__arrow"]}></div>
                     </div>
                 </div>
             </div>
