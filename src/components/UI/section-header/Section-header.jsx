17239910bb653f1a399d2c0018ce6976b872a47f
Alexandra <iammeduza@gmail.com>
2024-02-14 13:14:41 +0300

    fix: edit section header component for proper display

--- src/components/UI/section-header/Section-header.jsx
+++ src/components/UI/section-header/Section-header.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import styles from './Section-header.module.scss';
+import styles from './section-header.module.scss';
 import starsV1 from "../../../assets/images/stars-layer-v1.png";
 import starsV2 from "../../../assets/images/stars-layer-v2.png";
 
-const SectionHeader = (props) => {
-    const {content, version = '2'} = props;
+const SectionHeader = ({content, type = '2'}) => {
 
     return <div className={styles['section-header']}>
         <h2 className={styles['section-header__text']}>{content}</h2>
-        <img className={version === '1'? styles["section-header__decoration_shifted"] : styles["section-header__decoration"]} src={version === '1'? starsV1 : starsV2} alt=""></img>
+        <img className={type === '1'? styles["section-header__decoration_shifted"] : styles["section-header__decoration"]} src={type === '1'? starsV1 : starsV2} alt=""></img>
     </div>
 }
 
