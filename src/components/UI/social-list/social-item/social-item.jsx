39f4cb8abac4925e7f1c2e0e4b3cd5c29cf75626
Alexandra <iammeduza@gmail.com>
2024-02-16 20:01:41 +0300

    feat: implement state mosule as data source

--- src/components/UI/social-list/social-item/social-item.jsx
+++ src/components/UI/social-list/social-item/social-item.jsx
@@ -1,32 +1,20 @@
 import React from "react";
-import vk from "../../../../assets/icons/socials-vk-ico.svg";
-import inst from "../../../../assets/icons/socials-instagram-ico.svg";
-import tg from "../../../../assets/icons/socials-telegram-ico.svg";
 import styles from "./social-item.module.scss";
+import useImage from "../../../../custom-hooks/use-image";
 
-const SocialItem = (props) => {
-    const {link, name} = props;
-    
-    function getImage(name) {
-        const socials = {
-            'vkontakte': vk,
-            'instagram': inst,
-            'telegram': tg,
-        };
-
-        for(let [key, value] of Object.entries(socials)) {
-            if (key === name) {
-                return value;
-            }
-        }
-        return '';
-    }
+const SocialItem = ({link, name, iconPath}) => {
+    const {loading, image} = useImage(iconPath);
 
     return (
     <li className={styles["social-item"]}>
-        <a className={styles["social-item__link"]} href={link}>
-            <img className={styles["social-item__image"]} src={getImage(name)} alt={name}/>
-        </a>
+        {loading
+        ? "загрузка"
+        : (
+            <a className={styles["social-item__link"]} href={link}>
+                <img className={styles["social-item__image"]} src={image} alt={name}/>
+            </a>
+        )
+        }
     </li>
     )
 }
