61c7660d35d3f7126af9ca3e9980a6ea04fa66ed
Alexandra <iammeduza@gmail.com>
2024-02-16 19:58:30 +0300

    feat: implement state module as data source; move navigation icon item to separate component

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { Anchor } from "../UI/anchor/anchor";
 import styles from "./navigation-bar.module.scss";
-import logo from "../../assets/icons/plndsg-logo.svg";
+import { NavigationIco } from "./navigation-ico/navigation-ico";
 
-//TODO: move data to state if possible
 
 const AnchorGroup = ({anchors}) => {
     const [clickedId, setClickedId] = useState(-1);
@@ -12,28 +11,32 @@ const AnchorGroup = ({anchors}) => {
     return (
         <>
         {anchors.map((anchor, i) => {
+            const id = anchor.id;
             const anchorContent = anchor.content;
             const sectionId = anchor.sectionId;
             const type = anchor.type;
+            const icoPath = anchor.icoPath;
 
-            if (type === 'image') {
-                return <li className={styles["nav-bar__list-item"]} key={i}>
-                    <a id={i} className={styles["nav-bar__logo"]} href={'#' + sectionId} onClick={()=> setClickedId(i)}>
-                        <img className={styles["nav-bar__logo-img"]} src={logo} alt={anchorContent}/>
-                    </a>
-                </li>
+            if (type === "image") {
+                return (
+                    <NavigationIco key={id} content={anchorContent} sectionId={sectionId} icoPath={icoPath} id={i} setClickedId={setClickedId}/>
+                )
             }
-            return <li className={styles["nav-bar__list-item"]} key={i}><Anchor id={i} clickedId={clickedId} sectionId={sectionId} content={anchorContent} setClickedId={setClickedId}/></li>
+            return (
+                <li className={styles["nav-bar__list-item"]} key={id}>
+                    <Anchor id={i} clickedId={clickedId} sectionId={sectionId} content={anchorContent} setClickedId={setClickedId}/>
+                </li>
+            )
         })}
         </>
     )
 }
 
-export const NavigationBar = (props) => {
+export const NavigationBar = ({anchorsData}) => {
 
     return <nav className={styles["nav-bar"]}>
         <ul className={styles["nav-bar__list"]}>
-            <AnchorGroup anchors={[{content: "Полина Дизайн логотип", sectionId: "header", type: 'image'},{content: "преимущества", sectionId: "advantages", type: 'anchor'}, {content: "портфолио", sectionId: "portfolio", type: 'anchor'}, {content: "стоимость", sectionId: "prices", type: 'anchor'}]}/>
+            <AnchorGroup anchors={anchorsData}/>
         </ul>
     </nav>
 };
