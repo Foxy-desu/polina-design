e251fb78d1e35ab9c171c9322c39725eada5425c
Alexandra <iammeduza@gmail.com>
2024-02-16 19:55:16 +0300

    feat: implement state module as data sourse

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -4,30 +4,32 @@ import { PhotoFrame } from "../UI/photo-frame/photo-frame";
 import { EmojiCard } from "../UI/emoji-card/emoji-card";
 import { InformationBlock } from "../UI/information-block/information-block";
 
-export const Intro =({cardsData, infoBlockData})=> {
-    const plain = cardsData.plain;
-    const designed = cardsData.designed;
+export const Intro =({introData})=> {
+    const header = introData.sectionHeading;
+    const plain = introData.cards.plain;
+    const designed = introData.cards.designed;
+    const description = introData.description;
 
     return (
         <div className={styles["intro"]}>
-            <h1 className={styles["intro__header"]}>Карточки товаров</h1>
+            <h1 className={styles["intro__header"]}>{header.content}</h1>
             <div className={styles["intro__content-wrap"]}>
                 <div className={styles["intro__content"]}>
                     <div className={styles["cards"]}>
                         <div className={`${styles["cards__plain-card"]} ${styles["card-wrap"]}`}>
-                            <EmojiCard type="plain" content="до"/>
-                            <PhotoFrame path={plain.path} info={plain.info} type='plain'/>
+                            <EmojiCard type={plain.type} content={plain.cardDesc}/>
+                            <PhotoFrame path={plain.path} info={plain.info} type={plain.type}/>
                         </div>
                         <div className={styles["cards__transform-arrow"]}>
                         </div>
                         <div className={`${styles["cards__designed-card"]} ${styles["card-wrap"]}`}>
-                            <EmojiCard type="designed" content="после"/>
-                            <PhotoFrame path={designed.path} info={designed.info} type='designed'/>
+                            <EmojiCard type={designed.type} content={designed.cardDesc}/>
+                            <PhotoFrame path={designed.path} info={designed.info} type={designed.type}/>
+                            <div className={styles["intro__arrow"]}></div>
                         </div>
                     </div>
                     <div className={styles["intro__info-wrap"]}>
-                        <InformationBlock heading={infoBlockData.heading} description={infoBlockData.description} descType="large"/>
-                        <div className={styles["intro__arrow"]}></div>
+                        <InformationBlock heading={description.heading} description={description.description} descType={description.descType}/>
                     </div>
                 </div>
             </div>
