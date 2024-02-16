ec34e01631f82fe7d069e6cb2c33261856c3258e
Alexandra <iammeduza@gmail.com>
2024-02-16 19:59:25 +0300

    feat: create footer component

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./footer.module.scss";
+
+export const Footer =({footerData})=> {
+
+    function removeSpaces(string) {
+        return string.replace(/\s/g, "");
+    }
+
+    function renderContacts(contacts) {
+        return contacts.map((contact)=> {
+            const linkClass = `footer__link-${contact.type.slice(0,-1)}`
+            return (
+                <li key={contact.id} className="footer__list-item">
+                    <a className={styles[linkClass]} href={contact.type + (contact.type === "tel:"? removeSpaces(contact.content) : contact.content)}>{contact.content}</a>
+                </li>
+            )
+        })
+    }
+
+    return (
+        <footer className={styles["footer"]}>
+            <div className={styles["content-wrap"]}>
+                <ul className={styles["footer__list"]}>
+                    {renderContacts(footerData.contacts)}
+                </ul>
+            </div>
+        </footer>
+    )
+}
