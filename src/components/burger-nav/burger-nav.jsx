b013df3e744ea31ab35dfd0fe5523da0878ae5f8
Alexandra <iammeduza@gmail.com>
2024-02-21 23:27:59 +0300

    feat: create burger nav component

---
+++ src/components/burger-nav/burger-nav.jsx
@@ -0,0 +1,41 @@
+import React, {useCallback, useEffect, useState} from "react";
+import styles from "./burger-nav.module.scss";
+import {BurgerButton} from "../UI/burger-button/burger-button";
+import {NavigationBar} from "../navigation-bar/navigation-bar";
+import {SocialList} from "../UI/social-list/social-list";
+
+export const BurgerNav =({anchorsData, socialData, setPos= false})=> {
+    const [click, setClick] = useState("open");
+
+    const setClickWrap = useCallback((val)=> setClick(val), [setClick]);
+
+    useEffect(()=> {
+        if (setPos) setPos(click);
+        console.log(click)
+    }, [setPos, click]);
+
+    function clickHandler() {
+        if(click === "close") {
+            setClick("open")
+        }
+        else return;
+    }
+
+    return (
+        <div className={styles["navigation"]} >
+            <div className={styles["navigation__btn"]}>
+                <BurgerButton setClick={setClickWrap} click={click}/>
+            </div>
+            <div className={
+                click === "open"
+                    ? styles["navigation__menu"]
+                    : `${styles["navigation__menu"]} ${styles["navigation__menu_active"]}`
+            }>
+                <NavigationBar anchorsData={anchorsData} clickHandler={clickHandler}/>
+                <div className={styles["navigation__socials"]}>
+                    <SocialList socialData={socialData}  clickHandler={clickHandler}/>
+                </div>
+            </div>
+        </div>
+    )
+}
