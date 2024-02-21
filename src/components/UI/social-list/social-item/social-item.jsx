b3f3283074f4556b30599356136a2292c4ec77dd
Alexandra <iammeduza@gmail.com>
2024-02-21 23:32:01 +0300

    feat: implement onclick action for automatic navbar close

--- src/components/UI/social-list/social-item/social-item.jsx
+++ src/components/UI/social-list/social-item/social-item.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./social-item.module.scss";
 import useImage from "../../../../custom-hooks/use-image";
 
-const SocialItem = ({link, name, iconPath}) => {
+const SocialItem = ({link, name, iconPath, clickHandler= false}) => {
     const {loading, image} = useImage(iconPath);
 
+    function handleCheck() {
+        if (clickHandler) clickHandler();
+        else return;
+    }
     return (
-    <li className={styles["social-item"]}>
+    <li className={styles["social-item"]} onClick={handleCheck}>
         {loading
         ? "загрузка"
         : (
--- src/components/UI/social-list/social-list.jsx
+++ src/components/UI/social-list/social-list.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { SocialItem } from "./social-item/social-item";
 import styles from "./social-list.module.scss";
 
-export const SocialList = ({socialData}) => {
+export const SocialList = ({socialData, clickHandler}) => {
 
     function listItems(socialData) {
         return socialData.map((social) => {
-            return <SocialItem link={social.link} name={social.name} iconPath={social.iconPath}/>
+            return <SocialItem link={social.link} name={social.name} iconPath={social.iconPath} clickHandler={clickHandler}/>
         });
     }
 
