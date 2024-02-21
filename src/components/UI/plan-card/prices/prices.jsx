b6d6b27d998a7b99ed1f97c0591a9360ddf27a1d
Alexandra <iammeduza@gmail.com>
2024-02-21 12:40:24 +0300

    feat: implement empty previous price line for proper display

--- src/components/UI/plan-card/prices/prices.jsx
+++ src/components/UI/plan-card/prices/prices.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./prices.module.scss";
+import parse from "html-react-parser";
 
 const Prices = (props) => {
     const {price, prevPrice} = props;
 
     return <div className={styles["prices"]}>
         <p className={styles["prices__current"]}>{price + ' rub'}</p>
-        {prevPrice ? <p className={styles["prices__previous"]}>{prevPrice + ' rub'}</p> : false}
+        {prevPrice
+            ? <p className={styles["prices__previous"]}>{prevPrice + ' rub'}</p>
+            : <p className={`${styles["prices__previous"]} ${styles["prices__previous_transparent"]}`}>
+                {parse("&nbsp;")}
+            </p>
+        }
     </div>
 };
 
