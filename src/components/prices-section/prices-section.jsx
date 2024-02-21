0edd046d796a39d23a96261d2a4372880b42ce2d
Alexandra <iammeduza@gmail.com>
2024-02-21 12:41:11 +0300

    feat: implement card hover behaviour

--- src/components/prices-section/prices-section.jsx
+++ src/components/prices-section/prices-section.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./prices-section.module.scss";
 import { PlanCard } from "../UI/plan-card/plan-card";
 import { SectionHeader } from "../UI/section-header/section-header";
 import { Button } from "../UI/button/button";
+
 
 export const PricesSection = ({pricesData}) => {
     const header = pricesData.sectionHeading;
     const plans = pricesData.plans;
     const btn = pricesData.buttons[0];
 
+    const [hoverIndex, setHover] = useState(2);
+
     function renderPlans(plans) {
+
         return (
-            plans.map((plan)=> {
+            plans.map((plan, i)=> {
                 return (
-                <li key={plan.id} className={styles["prices__list-item"]}>
-                    <PlanCard planName={plan.planName} perks={plan.perks} price={plan.price} prevPrice={plan.prevPrice} discount={plan.discount} type={plan.type}/>
+                <li
+                    key={plan.id}
+                    className={ hoverIndex === i? styles["prices__list-item"] : `${styles["prices__list-item"]} ${styles["prices__list-item_darkened"]}`}>
+                    <PlanCard planName={plan.planName} perks={plan.perks} price={plan.price} prevPrice={plan.prevPrice} discount={plan.discount} type={plan.type} index={i} effects={{hoverIndex, setHover}}/>
                 </li>
                 )
             })
@@ -28,7 +34,7 @@ export const PricesSection = ({pricesData}) => {
                 <div className={styles["prices__header"]}>
                     <SectionHeader content={header.content} type={header.type}/>
                 </div>
-                <ul className={styles["prices__list"]}>
+                <ul className={styles["prices__list"]} onMouseLeave={()=>{setHover(2)}}>
                     {renderPlans(plans)}
                 </ul>
                 <div className={styles["prices__btn"]}>
