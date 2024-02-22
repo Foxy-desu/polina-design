204a33afcb25b8041eeb567aaa63492d5a835d21
Alexandra <iammeduza@gmail.com>
2024-02-22 14:43:35 +0300

    feat: implement click handler check

--- src/components/UI/anchor/Anchor.jsx
+++ src/components/UI/anchor/Anchor.jsx
@@ -4,9 +4,16 @@ import styles from './anchor.module.scss';
 const Anchor = (props) => {
     //activate .anchor_active using js//
     //activate on tabulation enter
-    const {sectionId, content, id, clickedId, setClickedId} = props;
+    const {sectionId, content, id, clickedId, setClickedId, clickHandler} = props;
+    function handleCheck() {
+        if (clickHandler) clickHandler();
+        else return;
+    }
     
-    return <div className={`${clickedId === id? styles.anchor + ' ' + styles.anchor_active : styles.anchor}`} onClick={()=> setClickedId(id)}>
+    return <div className={`${clickedId === id? styles.anchor + ' ' + styles.anchor_active : styles.anchor}`} onClick={()=> {
+        setClickedId(id);
+        handleCheck();
+    }}>
         <a className={styles.anchor__link} href={`#${sectionId}`}>{content}</a>
     </div>
 }
