a30baa13464cdcd4b7538a10b848287e2faeb114
Alexandra <iammeduza@gmail.com>
2024-02-23 20:01:13 +0300

    feat: add conditional carousel render

--- src/components/prices-section/prices-section.jsx
+++ src/components/prices-section/prices-section.jsx
@@ -1,26 +1,36 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./prices-section.module.scss";
 import { PlanCard } from "../UI/plan-card/plan-card";
 import { SectionHeader } from "../UI/section-header/section-header";
 import { Button } from "../UI/button/button";
+import {PricesCarousel} from "../prices-carousel/prices-carousel";
 
 
-export const PricesSection = ({pricesData}) => {
+export const PricesSection = ({pricesData, blockType}) => {
     const header = pricesData.sectionHeading;
     const plans = pricesData.plans;
     const btn = pricesData.buttons[0];
 
-    const [hoverIndex, setHover] = useState(2);
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 873;
+
+    useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+
+        window.addEventListener("resize", handleResizeWindow);
+        return ()=> {
+            window.removeEventListener("resize", handleResizeWindow);
+        }
+    }, []);
 
     function renderPlans(plans) {
-
         return (
             plans.map((plan, i)=> {
                 return (
                 <li
                     key={plan.id}
-                    className={ hoverIndex === i? styles["prices__list-item"] : `${styles["prices__list-item"]} ${styles["prices__list-item_darkened"]}`}>
-                    <PlanCard planName={plan.planName} perks={plan.perks} price={plan.price} prevPrice={plan.prevPrice} discount={plan.discount} type={plan.type} index={i} effects={{hoverIndex, setHover}}/>
+                    className={styles["prices__list-item"]}>
+                    <PlanCard planName={plan.planName} perks={plan.perks} price={plan.price} prevPrice={plan.prevPrice} discount={plan.discount} type={plan.type}/>
                 </li>
                 )
             })
@@ -34,9 +44,12 @@ export const PricesSection = ({pricesData}) => {
                 <div className={styles["prices__header"]}>
                     <SectionHeader content={header.content} type={header.type}/>
                 </div>
-                <ul className={styles["prices__list"]} onMouseLeave={()=>{setHover(2)}}>
-                    {renderPlans(plans)}
-                </ul>
+                { width > breakpoint
+                    ? (<ul className={styles["prices__list"]}>
+                        {renderPlans(plans)}
+                    </ul>)
+                    : <PricesCarousel plans={plans}/>
+                }
                 <div className={styles["prices__btn"]}>
                     <Button content={btn.content}/>
                 </div>
