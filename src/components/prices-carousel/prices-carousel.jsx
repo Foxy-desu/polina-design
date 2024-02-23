654c0341c066a7bb3fac695b6c2ca1732b2f39ca
Alexandra <iammeduza@gmail.com>
2024-02-23 19:56:58 +0300

    feat: create prices carousel component

---
+++ src/components/prices-carousel/prices-carousel.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {EffectCards, EffectCoverflow, Navigation, Pagination} from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/bundle';
+import {PlanCard} from "../UI/plan-card/plan-card";
+import styles from "./prices-carousel.module.scss";
+
+
+export const PricesCarousel = ({plans}) => {
+
+    function renderSwipes(plans) {
+
+        return (
+            plans.map((plan, i)=> {
+                return (
+                    <SwiperSlide>
+                        <div key={plan.id}
+                                     className={styles["plan-wrap"]}
+                        >
+                            <PlanCard planName={plan.planName} perks={plan.perks} price={plan.price} prevPrice={plan.prevPrice} discount={plan.discount} type={plan.type} shadow={"slight"}
+                            />
+                        </div>
+                    </SwiperSlide>
+                )
+            })
+        )
+    }
+
+    return (
+        <div className={styles["swiper-wrap"]}>
+            <Swiper effect={'coverflow'}
+                    loop={true}
+                    grabCursor={false}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper">
+                {renderSwipes(plans)}
+            </Swiper>
+        </div>
+
+    )
+}
