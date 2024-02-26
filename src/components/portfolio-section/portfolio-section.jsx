d3e7bf7742cffc2f657f144ee6d28349e67a3d49
Alexandra <iammeduza@gmail.com>
2024-02-26 18:26:29 +0300

    feat: implement conditional content display

--- src/components/portfolio-section/portfolio-section.jsx
+++ src/components/portfolio-section/portfolio-section.jsx
@@ -1,29 +1,85 @@
-import React from "react";
-import styles from "./portfolio-section.module.scss";
+import React, {useEffect, useState} from "react";
 import { SectionHeader } from "../UI/section-header/section-header";
 import { PhotoFrame } from "../UI/photo-frame/photo-frame";
 import { Button } from "../UI/button/button";
+import styles from "./portfolio-section.module.scss";
 
-export const PortfolioSection = ({portfolioData})=> {
+
+const ShowMoreBtn = ({isMinimized, setMinimized}) => {
+
+    const button = {
+        id: "portBtn0",
+        content: "показать больше карточек",
+        type: "",
+        action: ""
+    };
+    const [displayMode, setDisplayMode] = useState(isMinimized);
+
+    useEffect(()=> {
+        if(isMinimized === true) setDisplayMode(true);
+    }, [isMinimized])
+
+    return (
+        <div
+            key={button.id}
+            className={displayMode
+                ? styles["portfolio__btn-wrap"]
+                : styles["portfolio__btn-wrap"] + " " + styles["portfolio__btn-wrap_hidden"]}
+            onClick={()=> {
+                setMinimized(false);
+                setDisplayMode(false);
+            }}>
+            <Button content={button.content} type={button.type}/>
+        </div>
+    )
+}
+export const PortfolioSection = ({portfolioData}) => {
     const heading = portfolioData.sectionHeading;
     const cards = portfolioData.portfolioCards;
     const btns = portfolioData.buttons;
+    const breakpoint = 415;
+
+    const [width, setWidth] = useState(()=> {
+        return window.innerWidth
+    })
+    const [isMinimized, setMinimized] = useState(()=> {
+        return width <= breakpoint ? true : false;
+    })
+
+    useEffect(() => {
+        const windowResizeHandler = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', windowResizeHandler);
+        return ()=> {
+            window.removeEventListener('resize', windowResizeHandler);
+        }
+    }, []);
+    useEffect(()=> {
+        if(width <= breakpoint) setMinimized(true);
+    }, [width])
 
     function renderCards(cards) {
         return cards.map((card)=> {
-            return <div key={card.id} className={styles["portfolio__card-wrap"]}>
+            return <div key={card.id} className={
+                isMinimized
+                    ? styles["portfolio__card-wrap"] + ' ' + styles["portfolio__card-wrap_minimized"]
+                    : styles["portfolio__card-wrap"]}>
                 <PhotoFrame info={card.info} path={card.path}/>
             </div>
         })
     }
 
     function renderButtons(buttons) {
+
         return buttons.map((button)=> {
             return <div key={button.id} className={styles["portfolio__btn-wrap"]}>
                 <Button content={button.content} type={button.type}/>
             </div>
         })
-    }
+    };
+
     return (
         <section className={styles["portfolio"]} id="portfolio">
             <div className={styles["content-wrap"]}>
@@ -34,6 +90,7 @@ export const PortfolioSection = ({portfolioData})=> {
                     {renderCards(cards)}
                 </div>
                 <div className={styles["portfolio__btns"]}>
+                    <ShowMoreBtn setMinimized={setMinimized} isMinimized={isMinimized}/>
                     {renderButtons(btns)}
                 </div>
             </div>
