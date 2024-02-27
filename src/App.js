d7e27f2670294cce7de2481052821459e58092cc
Alexandra <iammeduza@gmail.com>
2024-02-27 19:24:43 +0300

    feat: add router wrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { state } from "./state/state";
 import { PageLayout } from "./components/page-layout/page-layout";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <PageLayout data={state}/>
-    </>
+    </BrowserRouter>
   );
 }
 
