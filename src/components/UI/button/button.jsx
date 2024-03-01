87a4d7e24c36df7ef9c2d307cb96501696391729
Alexandra <iammeduza@gmail.com>
2024-03-01 13:58:46 +0300

    feat: implement set state from local storage if exists on plan card button click

--- src/components/UI/button/button.jsx
+++ src/components/UI/button/button.jsx
@@ -3,9 +3,8 @@ import {Link} from 'react-router-dom';
 import styles from './button.module.scss';
 
 //TODO: the component is dirty. make it clear
-const Button =({content, type='', path})=> {
+const Button =({content, type='', path, btnId=false, addToLocal=false})=> {
     const additionalStyle = getStyle(checkType(type));
-
     function checkType(type) {
         const types = {
             colored: 'btn_colored',
@@ -21,8 +20,17 @@ const Button =({content, type='', path})=> {
         if(type === '') return '';
         return styles[type];
     };
-    
-    // return <button className={`${styles.btn}${' ' + additionalStyle}`} type='button'>{content}</button>
+    if (addToLocal !== false) {
+        return <Link
+                className={`${styles.btn}${' ' + additionalStyle}`}
+                to={path}
+                onClick={()=> {
+                addToLocal(btnId)}}
+                >
+                    {content}
+                </Link>
+    }
+
     return <Link className={`${styles.btn}${' ' + additionalStyle}`} to={path}>{content}</Link>
 };
 
--- src/components/order-form/order-form.jsx
+++ src/components/order-form/order-form.jsx
@@ -1,30 +1,38 @@
 import React, {useEffect, useState} from "react";
 import style from "./order-form.module.scss";
-import {Link} from "react-router-dom";
 import {Button} from "../UI/button/button";
 
-const ItemPicker = ({options, setSelectText})=> {
-
+const ItemPicker = ({options, setSelectText, selectText})=> {
     const [select, setSelect] = useState(()=> {
+        if(selectText !== "") return selectText;
         return options[0].id;
     });
-
     useEffect(()=> {
         setSelectText(select);
     }, [select])
 
     function renderOptions(options){
-        return options.map((option, i)=>{
-            return <option key={option.id} value={option.id}>{option.planName} ({option.price} руб.)</option>
+        return options.map((option)=>{
+            return (
+                <option
+                    className={style["options-list__option"]}
+                    key={option.id}
+                    value={option.id}
+                >
+                    {option.planName}
+                </option>
+            )
         })
     }
 
     return (
-        <label>
-            Выберите товар:
-            <select style={{background: "blue"}} value={select} onChange={(event)=> {
+        <label className={style["options-list__label"]}>
+            Выберите пакет услуг:<br/>
+            <select
+                className={style["options-list"]}
+                value={select} onChange={(event) => {
                 setSelect(event.target.value);
-            }} required={true}>
+            }}>
                 {renderOptions(options)}
             </select>
         </label>
@@ -33,14 +41,17 @@ const ItemPicker = ({options, setSelectText})=> {
 const PhoneInput = ({fieldText, setFieldText})=> {
 
     return (
-        <input
-            required
-            style={{background: "blue"}}
-            type="tel"
-            value={fieldText}
-            onChange={(event) => {
-            setFieldText(event.target.value)
-        }}/>
+        <label className={style["phone-input__label"]}>
+            Введите номер телефона <br/>
+            <input className={style["phone-input__input"]}
+                  required
+                  type="tel"
+                  value={fieldText}
+                  onChange={(event) => {
+                      setFieldText(event.target.value)
+                  }}
+            />
+        </label>
     )
 }
 const ItemDesc = ({selectText, options}) => {
@@ -52,18 +63,20 @@ const ItemDesc = ({selectText, options}) => {
 
             function renderPerks(perks) {
                 return perks.map((perk)=> {
-                    return <li key={perk.id}>{perk}</li>
+                    return <li className={style["perks__list-item"]} key={perk.id}>{perk}</li>
                 })
             }
 
             return (
                 <>
-                    <p>Вы выбрали план "{data.planName}"</p>
-                    <p>Стоимость: {data.price} рублей.</p>
-                    <p>Включенные услуги:</p>
-                    <ul>
-                        {renderPerks(data.perks)}
-                    </ul>
+                    <h3 className={style["description__heading"] + ' ' + style["heading"]}>Вы выбрали пакет "{data.planName}"</h3>
+                    <p className={style["description__price"]}>cтоимостью <span className={style["accent"]}>{data.price}</span> рублей.</p>
+                    <div className={style["description__perks"]}>
+                        <h3 className={style["perks-heading"] + ' ' + style["heading"]}>Включенные услуги:</h3>
+                        <ul className={style["perks-list"]}>
+                            {renderPerks(data.perks)}
+                        </ul>
+                    </div>
                 </>
             )
         }
@@ -99,14 +112,13 @@ const ItemPrice = ({selectText, options}) => {
 const Comment = ({commentText, setCommentText}) => {
 
     return (
-        <textarea
+        <textarea className={style["comment__textarea"]}
             rows="5"
             cols="33"
             value={commentText}
             placeholder={"Вы можете оставить комментарий к заказу"}
             onChange={(event)=> {
             setCommentText(event.target.value)}}
-            style={{color: "black"}}
         />
     )
 }
@@ -117,12 +129,21 @@ export const OrderForm = ({data}) => {
 
     const [blockType, setBlockType] = useState("form");
     const [fieldText, setFieldText] = useState("");
-    const [selectText, setSelectText] = useState("");
+    const [selectText, setSelectText] = useState(()=> {
+        if(localStorage.getItem('plan')) {
+            return (localStorage.getItem('plan'))
+        }
+        else return ""
+    });
     const [commentText, setCommentText] = useState("");
 
     useEffect(() => {
-        console.log(`selected plan is ${selectText}`)
+        console.log(`selected plan is ${selectText}`);
     }, [selectText]);
+
+    useEffect(()=> {
+        return ()=> localStorage.removeItem("plan");
+    }, []);
 
     function makeOrder(options, fieldText, selectText, commentText) {
         function prepareObject() {
@@ -139,6 +160,7 @@ export const OrderForm = ({data}) => {
         if(fieldText) {
             console.log(prepareObject());
             setBlockType("alert");
+            localStorage.removeItem("plan");
         }
         else window.alert("Пожалуйста, введите номер телефона")
 
@@ -147,20 +169,39 @@ export const OrderForm = ({data}) => {
     return (
         <>
             {blockType === "alert"
-                ? <div>Information was sent. We will contact you shortly</div>
+                ? <div className={style["message"]}>
+                    <p className={style["message__text"]}>Спасибо за предоставленную информацию. Временно она помещена в логи. Позднее будет
+                        имплементирован функционал отправки данных дизайнеру</p>
+                    <div className={style["message__button"]}>
+                        <Button content={"на главную"} path={"/"}></Button>
+                    </div>
+                </div>
                 : (
-                    <form>
-                        <ItemPicker options={options} setSelectText={setSelectText}/>
-                        <PhoneInput fieldText={fieldText} setFieldText={setFieldText}/>
-                        <ItemPrice selectText={selectText} options={options} />
-                        <ItemDesc selectText={selectText} options={options}/>
-                        <Comment commentText={commentText} setCommentText={setCommentText}/>
-                        <div onClick={() => {
-                            makeOrder(options, fieldText, selectText, commentText)
-                        }}>
-                            <Button content={"отправить данные"} type={"colored"}/>
-                        </div>
-                        <Button content={"Отмена"} to={"/"}></Button>
+                    <form className={style["form"]}>
+                        <div className={style["form__item-picker"]}>
+                        <ItemPicker options={options} setSelectText={setSelectText} selectText={selectText}/>
+                            </div>
+                            <div className={style["form__phone-input"]}>
+                                <PhoneInput fieldText={fieldText} setFieldText={setFieldText}/>
+                            </div>
+                            <div className={style["form__description"]}>
+                                <ItemDesc selectText={selectText} options={options}/>
+                            </div>
+                            <div className={style["form__comment"]}>
+                                <Comment commentText={commentText} setCommentText={setCommentText}/>
+                            </div>
+                            <div className={style["form__buttons"]}>
+                                <div
+                                    className={style["form__button"]}
+                                    onClick={() => {
+                                        makeOrder(options, fieldText, selectText, commentText)
+                                    }}>
+                                    <Button content={"отправить данные"} type={"colored"}/>
+                                </div>
+                                <div className={style["form__button"]}>
+                                    <Button content={"отмена"} path={"/"}></Button>
+                                </div>
+                            </div>
                     </form>
                 )
             }
