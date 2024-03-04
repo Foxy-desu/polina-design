30cc07700335a6f7c5f825c91c7abf0dd46c0ba9
Sasha <iammeduza@gmail.com>
2024-03-05 00:55:59 +0300

    feat: implement link change color if its section is invisible after click and scroll

--- src/components/UI/anchor/anchor.jsx
+++ src/components/UI/anchor/anchor.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import {HashLink as Link} from "react-router-hash-link";
 import styles from './anchor.module.scss';
 
-const Anchor = (props) => {
-    //activate .anchor_active using js//
-    //activate on tabulation enter
-    const {sectionId, content, id, clickedId, setClickedId, clickHandler} = props;
+const Anchor = ({sectionId, content, id, clickedId, setClickedId, clickHandler, visibleSections}) => {
+
+const [isVisible, setIsVisible] = useState(()=> {
+    if(visibleSections.includes(sectionId)) return true;
+    return false
+})
+
+    useEffect(()=> {
+        const section = sectionId;
+        if(visibleSections.includes(section)) {
+            setIsVisible( true);
+        }else setTimeout(()=> {
+            setIsVisible(false);
+        }, 150);
+    })
     function handleCheck() {
         if (clickHandler) clickHandler();
         else return;
     }
-    
-    return <div className={`${clickedId === id? styles["anchor"] + ' ' + styles["anchor_active"] : styles["anchor"]}`} onClick={()=> {
+    return <div className={`${isVisible===true && clickedId === id? styles["anchor"] + ' ' + styles["anchor_active"] : styles["anchor"]}`} onClick={()=> {
         setClickedId(id);
         handleCheck();
     }}>
