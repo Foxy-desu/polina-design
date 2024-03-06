ee4fcfaeddbcad69e3a604693be8ce30b1db912a
Sasha <iammeduza@gmail.com>
2024-03-06 22:54:19 +0300

    refactor: move use effect functions to separate files

---
+++ src/custom-hooks/use-intersection-observer.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from "react";
+
+export function useIntersectionObserver(section, setIsVisible, threshold) {
+  useEffect(()=> {
+
+    const observer = new IntersectionObserver(
+      ([entry])=> {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: threshold,
+      }
+    );
+
+    if(section.current) {
+      observer.observe(section.current);
+    }
+
+    return ()=> {
+      if(section.current) {
+        observer.unobserve(section.current);
+      }
+    };
+  }, []);
+}
---
+++ src/custom-hooks/use-visible-sections.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from "react";
+
+export function useVisibleSections(section, visibleSections, isVisible, setVisibleSections) {
+
+  useEffect(()=> {
+    const key = section.current.id;
+    const index = visibleSections.indexOf(key);
+    if(isVisible) {
+      if(index === -1) {
+        setVisibleSections([...visibleSections, key])
+      }
+    }if(!isVisible){
+      if(index > -1) {
+        setVisibleSections(visibleSections.filter((elem)=> {
+          return elem !== key;
+        }));
+      }
+    }
+  }, [isVisible]);
+
+}
